c31522f75107a4776d1d9b87668b5059746f1b15
Bruce Sun <bruce.sun@tubemogul.com>
2017-08-09 14:59:28 +0800

    Use theme mdui.

--- init_hexo.sh
+++ init_hexo.sh
@@ -1,9 +1,1 @@
-yarn add hexo hexo-generator-archive hexo-generator-category hexo-generator-index hexo-generator-tag hexo-renderer-ejs hexo-renderer-stylus hexo-renderer-marked hexo-server hexo-renderer-less hexo-generator-feed hexo-generator-json-content hexo-deployer-git
-
-# Enable tags page
-hexo new page tags
-# Then edit metadata in source/tags/index.md:
-#layout: tags
-#noDate: true
-#comments: false
-#---
+yarn add hexo hexo-generator-search hexo-generator-archive hexo-generator-category hexo-generator-index hexo-generator-tag hexo-helper-qrcode hexo-renderer-ejs hexo-renderer-stylus hexo-renderer-marked hexo-server hexo-renderer-less hexo-generator-feed hexo-generator-json-content hexo-deployer-git hexo-wordcount
--- init_theme_indigo.sh
+++ init_theme_indigo.sh
@@ -2,4 +2,18 @@
 git remote add -f theme-indigo https://github.com/yscoder/hexo-theme-indigo.git
 
 # Add subtree
-git subtree add --prefix themes/indigo theme-indigo master --squash
+git subtree add --prefix themes/indigo theme-indigo card --squash
+
+# Enable tags page
+hexo new page tags
+# Then edit metadata in source/tags/index.md:
+# layout: tags
+# comments: false
+# ---
+
+# Enable categories page
+hexo new page categories
+# Then edit metadata in source/categories/index.md:
+# layout: categories
+# comments: false
+# ---
